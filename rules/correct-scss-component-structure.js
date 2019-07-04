6cc596d7d209bc135b6647e61135d9f63594ce38
k.kravchenko <kostiantyn.Kravchenko@spryker.com>
2019-07-04 15:54:22 +0300

    te-3767 - rules for correct first scss mixin name in component

--- rules/correct-scss-component-structure.js
+++ rules/correct-scss-component-structure.js
@@ -20,11 +20,25 @@ module.exports = class extends Rule {
                     this.outcome.addError(`There is wrong name of scss file in ${type} ${bold(name)}:\n${dim(path)}`);
                 }
 
-                mixins.forEach(mixin => {
+                mixins.forEach((mixin, index) => {
                     const contentRule = '@content';
+                    const moduleName = component.module;
+                    const letterModuleArray = moduleName.split('');
+                    const convertedLetterModuleArray = letterModuleArray.map((letter, index) => {
+                        if (letter !== letter.toLowerCase() && index) {
+                            return `-${letter.toLowerCase()}`;
+                        }
+
+                        return letter;
+                    });
                     const { name: mixinName, hasContent, tree } = mixin;
                     const convertedMixinText = stringify(tree);
                     const contentRuleIndex = convertedMixinText.indexOf(contentRule);
+                    const convertedModuleName = convertedLetterModuleArray.join('').toLowerCase();
+
+                    if (!index && mixinName !== `${convertedModuleName}-${name}`) {
+                        this.outcome.addError(`name of the first mixin in file should consist of module name and component name in ${type} ${bold(name)}:\n${dim(path)}`);
+                    }
 
                     if (!mixinName.includes(name)) {
                         this.outcome.addError(`There is wrong name of mixin in scss file of ${type} ${bold(name)}:\n${dim(path)}`);
@@ -35,7 +49,7 @@ module.exports = class extends Rule {
                     }
 
                     if (hasContent && contentRuleIndex !== convertedMixinText.lastIndexOf(contentRule)) {
-                        this.outcome.addError(`It should be only one ${rule} rule in mixin in ${type} ${bold(name)}:\n${dim(path)}`);
+                        this.outcome.addError(`It should be only one ${contentRule} rule in mixin in ${type} ${bold(name)}:\n${dim(path)}`);
                     }
 
                     if (hasContent && /[a-z]+/.test(convertedMixinText.slice(contentRuleIndex + contentRule.length))) {
