20ebc448297928f654511f3c1c2ec628a6460074
Aleksey Belan <alexey.belan@spryker.com>
2019-07-12 15:35:29 +0300

    TE-3784: PR fixes

--- rules/mandatory-z-index-variable.js
+++ rules/mandatory-z-index-variable.js
@@ -10,19 +10,21 @@ module.exports = class extends Rule {
         data.components.forEach(component => {
             const { sass } = component.files;
 
-            if (sass.exists) {
-                const zIndexRegularExpression = /z-index:[\sA-Za-z0-9-\+\$]+[;}]/gi;
-                const zIndexRules = sass.content.match(zIndexRegularExpression);
+            if (!sass.exists) {
+                return;
+            }
 
-                if (zIndexRules) {
-                    zIndexRules.forEach(rule => {
-                       const zIndexValue = rule.slice(rule.indexOf(':') + 1);
+            const zIndexRegularExpression = /z-index:[\sA-Za-z0-9-\+\$]+[;}]/gi;
+            const zIndexRules = sass.content.match(zIndexRegularExpression);
 
-                       if (!isNaN(parseInt(zIndexValue))) {
-                           this.outcome.addError(`Value of z-index property shouldn't be a number in ${component.type} ${bold(component.name)}:\n${dim(component.path)}`);
-                       }
-                    });
-                }
+            if (zIndexRules) {
+                zIndexRules.forEach(rule => {
+                    const zIndexValue = rule.slice(rule.indexOf(':') + 1);
+
+                    if (!isNaN(parseInt(zIndexValue))) {
+                        this.outcome.addError(`Value of z-index property shouldn't be a number in ${component.type} ${bold(component.name)}:\n${dim(component.path)}`);
+                    }
+                });
             }
         });
     }
