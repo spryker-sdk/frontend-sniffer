3b3991338aa83455737f0eb9d53eae29c118ed09
Aleksey Belan <alexey.belan@spryker.com>
2019-07-12 16:17:20 +0300

    TE-3784: PR fixes

--- rules/mandatory-z-index-variable.js
+++ rules/mandatory-z-index-variable.js
@@ -17,15 +17,17 @@ module.exports = class extends Rule {
             const zIndexRegularExpression = /z-index:[\sA-Za-z0-9-\+\$]+[;}]/gi;
             const zIndexRules = sass.content.match(zIndexRegularExpression);
 
-            if (zIndexRules) {
-                zIndexRules.forEach(rule => {
-                    const zIndexValue = rule.slice(rule.indexOf(':') + 1);
-
-                    if (!isNaN(parseInt(zIndexValue))) {
-                        this.outcome.addError(`Value of z-index property shouldn't be a number in ${component.type} ${bold(component.name)}:\n${dim(component.path)}`);
-                    }
-                });
+            if (!zIndexRules) {
+                return;
             }
+
+            zIndexRules.forEach(rule => {
+                const zIndexValue = rule.slice(rule.indexOf(':') + 1);
+
+                if (!isNaN(parseInt(zIndexValue))) {
+                    this.outcome.addError(`Value of z-index property shouldn't be a number in ${component.type} ${bold(component.name)}:\n${dim(component.path)}`);
+                }
+            });
         });
     }
 }
