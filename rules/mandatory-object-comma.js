f79238e2db61ef560ac49a025ba9f5907cb8302f
k.kravchenko <kostiantyn.Kravchenko@spryker.com>
2019-09-05 12:28:05 +0300

    te-3932: cleaning content from twig variables brackets

--- rules/mandatory-object-comma.js
+++ rules/mandatory-object-comma.js
@@ -11,12 +11,14 @@ module.exports = class extends Rule {
         this.filterModulesData(data, 'twig').forEach(twigFileData => {
             const {files, type, name} = twigFileData;
             const {twig} = files;
+            const {content} = twig;
             const {path, api} = twig;
             const stringsBetweenBrackets = [];
             const betweenBlocksReg = /^\}[\s]{0,}\{$/i;
-            let dataArray = [];
+            const twigVariableReg = /\{\{[a-zA-Z0-9 _\=\.\"\%\':\/\_\-\~\,\(\)\?\|\[\]\n\r]{1,}\}\}/gi;
+            const twigVariables = content.match(twigVariableReg);
             const clearArrayFromBlocksData = stringsArray => stringsArray.filter(
-                string => !string.includes('{%') && !betweenBlocksReg.test(string));
+                string => !string.includes('{%') && !string.includes('%}') && !betweenBlocksReg.test(string));
 
             const findStringBetweenBrackets = (string, stringsBetweenBrackets) => {
                 const reg = /(\[|\]|\{|\})[a-zA-Z0-9 _\=\.\"\%\':\-\~\,\(\)\?\n\r]{1,}(\[|\]|\{|\})/i;
@@ -40,10 +42,31 @@ module.exports = class extends Rule {
                 findStringBetweenBrackets(stringAfterBracketsPair, stringsBetweenBrackets);
             };
 
-            findStringBetweenBrackets(twig.content, stringsBetweenBrackets);
-            dataArray = clearArrayFromBlocksData(stringsBetweenBrackets);
+            const changeTwigVariablesToUnicode = (content, variables) => {
+                let modifiedContent = content;
+
+                if (Array.isArray(variables)) {
+                    variables.forEach(variable => {
+                        const changedStringSymbolLength = 2;
+
+                        modifiedContent = modifiedContent.replace(
+                            variable, `&#123;&#123;${variable.slice(
+                                changedStringSymbolLength,(-1)*changedStringSymbolLength)}&#125;&#125;`);
+                    })
+                }
+
+                return modifiedContent;
+            };
+
+            const modifiedContent = changeTwigVariablesToUnicode(content, twigVariables);
+
+            findStringBetweenBrackets(modifiedContent, stringsBetweenBrackets);
+
+            const dataArray = clearArrayFromBlocksData(stringsBetweenBrackets);
+
             console.log(path);
             console.log(dataArray);
+
         })
     }
 };
